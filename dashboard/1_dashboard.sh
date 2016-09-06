9a027037e511ef26bf50f7234a692446acdbd220
admin <admin@localhost.localdomain>
2016-09-06 21:00:33 +0700

    commit

--- dashboard/1_dashboard.sh
+++ dashboard/1_dashboard.sh
@@ -8,6 +8,7 @@ if [ -f local_settings ]; then
     rm -rf /etc/openstack-dashboard/local_settings
     cp local_settings /etc/openstack-dashboard/local_settings
 fi
+chown root:apache /etc/openstack-dashboard/local_settings
 systemctl restart httpd.service memcached.service
 
 echo "login to 10.0.0.11/dashboard"
